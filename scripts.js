f2c4726f9066553a5615b55e607431cabc3abd67
Luiz Fernando <lfaires@gmail.com>
2021-04-03 13:09:59 -0300

    botão fechar pedido ativo

--- scripts.js
+++ scripts.js
@@ -1,31 +1,53 @@
+let food = 0;
+let beverage = 0;
+let dessert = 0;
+
 function activeOrderFood(classOrder) {
     const active = document.querySelector(".order-food .activeli");
     
     if(active !== null){
-        active.classList.remove("activeli")
+        active.classList.remove("activeli");
     }
 
-    const selector = document.querySelector("." + classOrder)
-    selector.classList.add("activeli")
+    const selector = document.querySelector("." + classOrder);
+    selector.classList.toggle("activeli");
+    food = 1;
+    count(food, beverage, dessert)
 }
 
 function activeOrderBeverage(classOrder) {
     const active = document.querySelector(".order-beverage .activeli");
-    const selector = document.querySelector("."+ classOrder)
+        
     if(active !== null){
-        active.classList.remove("activeli")
+        active.classList.remove("activeli");
     }
-    
-    selector.classList.add("activeli")
+
+    const selector = document.querySelector("."+ classOrder);
+    selector.classList.add("activeli");
+    beverage = 1;
+    count(food, beverage, dessert)
 }
 
 function activeOrderDessert(classOrder) {
     const active = document.querySelector(".order-dessert .activeli");
-    const selector = document.querySelector("." + classOrder)
+    
     if(active !== null){
         active.classList.remove("activeli")
     }
-    
-    selector.classList.add("activeli")
+
+    const selector = document.querySelector("." + classOrder);
+    selector.classList.add("activeli");
+    dessert =1;
+    count(food, beverage, dessert)
+}
+
+function count(countFood, countBeverage, countDessert){
+    const countTotal = countFood + countBeverage + countDessert;
+    if (countTotal === 3) {
+    const selectButton = document.querySelector(".select-item")
+    selectButton.classList.add("hide")
+    const confirmButton = document.querySelector(".end-order")
+    confirmButton.classList.replace("hide","unhide")
+}
 }
 
