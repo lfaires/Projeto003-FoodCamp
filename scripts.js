6b39fbfc017929c43f351cebc2a24dd48b657129
Luiz Fernando <lfaires@gmail.com>
2021-04-04 18:33:23 -0300

    caixade confirmação finalizada

--- scripts.js
+++ scripts.js
@@ -4,13 +4,15 @@ let dessert = 0;
 
 function activeOrderFood(classOrder) {
     const active = document.querySelector(".order-food .activeli");
-    
+
     if(active !== null){
         active.classList.remove("activeli");
+
     }
 
-    const selector = document.querySelector("." + classOrder);
+    const selector = document.querySelector(classOrder);
     selector.classList.toggle("activeli");
+
     food = 1;
     count(food, beverage, dessert)
 }
@@ -51,3 +53,29 @@ function count(countFood, countBeverage, countDessert){
 }
 }
 
+function confirmOrder(){
+    
+    document.querySelector(".food").innerHTML = document.querySelector(".order-food .activeli .dishes").innerHTML;
+
+    document.querySelector(".food-price").innerHTML = "&#82;&#36; " + document.querySelector(".order-food .activeli .price").innerHTML;
+
+    document.querySelector(".beverage").innerHTML = document.querySelector(".order-beverage .activeli .dishes").innerHTML;
+
+    document.querySelector(".beverage-price").innerHTML = "&#82;&#36; " + document.querySelector(".order-beverage .activeli .price").innerHTML;
+
+    document.querySelector(".dessert").innerHTML = document.querySelector(".order-dessert .activeli .dishes").innerHTML; 
+
+    document.querySelector(".dessert-price").innerHTML = "&#82;&#36; " + document.querySelector(".order-dessert .activeli .price").innerHTML;
+
+    const total = (parseFloat(document.querySelector(".order-food .activeli .price").innerHTML) + parseFloat(document.querySelector(".order-beverage .activeli .price").innerHTML) + parseFloat(document.querySelector(".order-dessert .activeli .price").innerHTML)).toFixed(2)
+
+    document.querySelector(".total").innerHTML = "&#82;&#36; " + total
+    
+    const blank = document.querySelector(".blank")
+    blank.classList.replace("hide", "unhide")
+}
+
+function cancelOrder(){
+    const blank = document.querySelector(".blank")
+    blank.classList.replace("unhide", "hide")
+}
